bf755964d0031ee7e9c10bfb8085837f1d002b2a
Bruss Acevedo <bruss.acevedo@gmail.com>
2024-05-20 21:14:34 -0400

    Codigo en default y ordenado.

--- src/desafioEvaluado18V2/Main.java
+++ src/desafioEvaluado18V2/Main.java
@@ -1,6 +1,5 @@
 package desafioEvaluado18V2;
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,57 +10,54 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner scan = new Scanner(System.in);
-
-		ArrayList<String> lista = listaEntrada("src/desafioEvaluado18V2/entrada.txt");		
+		// Archivo (entrada.txt) de entrada ubicado en la carpeta src/desafioEvaluado18V2
+		ArrayList<String> lista = listaEntrada("src/desafioEvaluado18V2/entrada.txt");
+		
 		System.out.println("Porfavor indica el elemento a buscar;");
 		String busqueda = scan.nextLine();
 		scan.close();
-		
+
 		String directorio = ("src/" + "directorio");
 		String fichero = (directorio + "/" + "fichero.txt");
 		crearArchivo(directorio, fichero, lista);
+		
 		int contador = buscarTexto(fichero, busqueda);
-		if(contador > 0) {
-			System.out.println("cantidad de repeticiones del texto -> "+contador);
-		}else {
+		if (contador > 0) {
+			System.out.println("cantidad de repeticiones del texto -> " + contador);
+		} else {
 			System.out.println("No se encuentra el dato solicitado");
 		}
-		
 
 	}
-	
-	
-	public static ArrayList<String> listaEntrada(String ubicacionDelArchivo){
+
+	public static ArrayList<String> listaEntrada(String ubicacionDelArchivo) {
 		ArrayList<String> lista = new ArrayList<String>();
-		File dir = new File (ubicacionDelArchivo);
+		File dir = new File(ubicacionDelArchivo);
 		if (dir.exists()) {
-			
-			try (FileReader fr = new FileReader(dir); BufferedReader br = new BufferedReader(fr);){
+
+			try (FileReader fr = new FileReader(dir); BufferedReader br = new BufferedReader(fr);) {
 				String leerLinea = br.readLine();
 				while (leerLinea != null) {
 					lista.add(leerLinea);
 					leerLinea = br.readLine();
-				}	
-				
+				}
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}		
+			}
 		}
-		
+
 		return lista;
 	}
-	
-	
 
 	public static void crearArchivo(String directorio, String fichero, ArrayList<String> lista) {
 		File dir = new File(directorio);
@@ -88,7 +84,7 @@ public class Main {
 		}
 
 		try (FileWriter fw = new FileWriter(fichero); BufferedWriter bw = new BufferedWriter(fw);) {
-		
+
 			Iterator<String> iterador = lista.iterator();
 
 			while (iterador.hasNext()) {
@@ -103,37 +99,31 @@ public class Main {
 		}
 
 	}
-	
+
 	public static int buscarTexto(String fichero, String texto) {
-		ArrayList <String> listaBusqueda = new ArrayList<String>();
+		ArrayList<String> listaBusqueda = new ArrayList<String>();
 		File file = new File(fichero);
 		int contador = 0;
-		if(file.exists()) {
-			
-			try (FileReader fr = new FileReader(file);BufferedReader br = new BufferedReader(fr)) {
-				
+		if (file.exists()) {
+
+			try (FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
 				String leerLinea = br.readLine();
+
 				while (leerLinea != null) {
-					
 					listaBusqueda.add(leerLinea);
-					leerLinea = br.readLine();	
-					
-				}		
-				
+					leerLinea = br.readLine();
+				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			List<String> listaFiltrada = listaBusqueda.stream().filter(x-> x.equalsIgnoreCase(texto)).collect(Collectors.toList());
+			List<String> listaFiltrada = listaBusqueda.stream().filter(x -> x.equalsIgnoreCase(texto)).collect(Collectors.toList());
 			contador = listaFiltrada.size();
-			
-			
-		}else {
+
+		} else {
 			System.out.println("El fichero ingresado no existe.");
 		}
-
 		return contador;
 	}
-	
+
 }
